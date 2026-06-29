0d07389d1866a47c24c7d02f5eddd1c676ced873
Mariano Riefolo <rfmariano@protonmail.com>
2026-06-29 08:48:00 +0200

    style(api): fix ktfmt formatting in InPostItalyDeliveryService

--- app/src/main/java/dev/itsvic/parceltracker/api/InPostItalyDeliveryService.kt
+++ app/src/main/java/dev/itsvic/parceltracker/api/InPostItalyDeliveryService.kt
@@ -5,11 +5,11 @@ package dev.itsvic.parceltracker.api
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 import dev.itsvic.parceltracker.R
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import retrofit2.Retrofit
 import retrofit2.http.GET
 import retrofit2.http.Path
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 
 object InPostItalyDeliveryService : DeliveryService {
   override val nameResource: Int = R.string.service_inpost_it
@@ -19,12 +19,11 @@ object InPostItalyDeliveryService : DeliveryService {
   private const val BASE_URL = "https://api-shipx-it.easypack24.net/v1/"
 
   private val retrofit =
-    Retrofit
-      .Builder()
-      .baseUrl(BASE_URL)
-      .client(api_client)
-      .addConverterFactory(api_factory)
-      .build()
+      Retrofit.Builder()
+          .baseUrl(BASE_URL)
+          .client(api_client)
+          .addConverterFactory(api_factory)
+          .build()
 
   private val service = retrofit.create(API::class.java)
 
@@ -34,113 +33,105 @@ object InPostItalyDeliveryService : DeliveryService {
   }
 
   override suspend fun getParcel(
-    trackingId: String,
-    postalCode: String?,
+      trackingId: String,
+      postalCode: String?,
   ): Parcel {
     val response =
-      try {
-        service.getParcel(trackingId)
-      } catch (_: Exception) {
-        throw ParcelNonExistentException()
-      }
+        try {
+          service.getParcel(trackingId)
+        } catch (_: Exception) {
+          throw ParcelNonExistentException()
+        }
 
     return Parcel(
-      response.trackingNumber,
-      eventsToHistory(response.trackingDetails),
-      statusToEnum(response.trackingDetails.firstOrNull()?.status ?: "unknown"),
+        response.trackingNumber,
+        eventsToHistory(response.trackingDetails),
+        statusToEnum(response.trackingDetails.firstOrNull()?.status ?: "unknown"),
     )
   }
 
   private fun statusToEnum(status: String): Status =
-    when (status) {
-      "created" -> Status.Preadvice
+      when (status) {
+        "created" -> Status.Preadvice
 
-      "confirmed" -> Status.InTransit
+        "confirmed" -> Status.InTransit
 
-      "dispatched_by_sender",
-      "dispatched_by_sender_to_pok",
-      "collected_from_sender",
-      -> Status.InTransit
+        "dispatched_by_sender",
+        "dispatched_by_sender_to_pok",
+        "collected_from_sender", -> Status.InTransit
 
-      "taken_by_courier",
-      "taken_by_courier_from_pok",
-      "taken_by_courier_from_customer_service_point",
-      -> Status.PickedUpByCourier
+        "taken_by_courier",
+        "taken_by_courier_from_pok",
+        "taken_by_courier_from_customer_service_point", -> Status.PickedUpByCourier
 
-      "adopted_at_source",
-      "adopted_at_source_branch",
-      "adopted_at_sorting_center",
-      "stack_in_customer_service_point",
-      "stack_in_box_machine",
-      -> Status.InWarehouse
+        "adopted_at_source",
+        "adopted_at_source_branch",
+        "adopted_at_sorting_center",
+        "stack_in_customer_service_point",
+        "stack_in_box_machine", -> Status.InWarehouse
 
-      "sent_from_source_branch",
-      "unstack_from_customer_service_point",
-      "unstack_from_box_machine",
-      -> Status.InTransit
+        "sent_from_source_branch",
+        "unstack_from_customer_service_point",
+        "unstack_from_box_machine", -> Status.InTransit
 
-      "out_for_delivery",
-      "out_for_delivery_to_address",
-      -> Status.OutForDelivery
+        "out_for_delivery",
+        "out_for_delivery_to_address", -> Status.OutForDelivery
 
-      "ready_to_pickup",
-      "ready_to_pickup_from_branch",
-      "ready_to_pickup_from_pok",
-      "courier_avizo_in_customer_service_point",
-      -> Status.AwaitingPickup
+        "ready_to_pickup",
+        "ready_to_pickup_from_branch",
+        "ready_to_pickup_from_pok",
+        "courier_avizo_in_customer_service_point", -> Status.AwaitingPickup
 
-      "pickup_reminder_sent",
-      "pickup_reminder_sent_address",
-      -> Status.PickupTimeEndingSoon
+        "pickup_reminder_sent",
+        "pickup_reminder_sent_address", -> Status.PickupTimeEndingSoon
 
-      "returned_to_sender" -> Status.ReturnedToSender
+        "returned_to_sender" -> Status.ReturnedToSender
 
-      "delay_in_delivery" -> Status.Delayed
+        "delay_in_delivery" -> Status.Delayed
 
-      "redirect_to_box" -> Status.Readdressed
+        "redirect_to_box" -> Status.Readdressed
 
-      "rejected_by_receiver",
-      "undelivered",
-      "undelivered_wrong_address",
-      "undelivered_cod_cash_receiver",
-      "pickup_time_expired",
-      "stack_parcel_pickup_time_expired",
-      "stack_parcel_in_box_machine_pickup_time_expired",
-      "canceled_redirect_to_box",
-      -> Status.DeliveryFailure
+        "rejected_by_receiver",
+        "undelivered",
+        "undelivered_wrong_address",
+        "undelivered_cod_cash_receiver",
+        "pickup_time_expired",
+        "stack_parcel_pickup_time_expired",
+        "stack_parcel_in_box_machine_pickup_time_expired",
+        "canceled_redirect_to_box", -> Status.DeliveryFailure
 
-      "delivered" -> Status.Delivered
+        "delivered" -> Status.Delivered
 
-      else -> logUnknownStatus("InPostIT", status)
-    }
+        else -> logUnknownStatus("InPostIT", status)
+      }
 
   private fun eventsToHistory(details: List<TrackingDetail>): List<ParcelHistoryItem> =
-    details.map { item ->
-      ParcelHistoryItem(
-        item.status.replace('_', ' ').replaceFirstChar { it.uppercase() },
-        LocalDateTime.parse(item.datetime, DateTimeFormatter.ISO_DATE_TIME),
-        item.agency ?: "",
-      )
-    }
+      details.map { item ->
+        ParcelHistoryItem(
+            item.status.replace('_', ' ').replaceFirstChar { it.uppercase() },
+            LocalDateTime.parse(item.datetime, DateTimeFormatter.ISO_DATE_TIME),
+            item.agency ?: "",
+        )
+      }
 
   private interface API {
     @GET("tracking/{trackingId}")
     suspend fun getParcel(
-      @Path("trackingId") trackingId: String,
+        @Path("trackingId") trackingId: String,
     ): TrackingResponse
   }
 
   @JsonClass(generateAdapter = true)
   internal data class TrackingResponse(
-    @Json(name = "tracking_number") val trackingNumber: String,
-    @Json(name = "tracking_details") val trackingDetails: List<TrackingDetail>,
+      @Json(name = "tracking_number") val trackingNumber: String,
+      @Json(name = "tracking_details") val trackingDetails: List<TrackingDetail>,
   )
 
   @JsonClass(generateAdapter = true)
   internal data class TrackingDetail(
-    @Json(name = "origin_status") val originStatus: String,
-    val status: String,
-    val agency: String?,
-    val datetime: String,
+      @Json(name = "origin_status") val originStatus: String,
+      val status: String,
+      val agency: String?,
+      val datetime: String,
   )
 }
